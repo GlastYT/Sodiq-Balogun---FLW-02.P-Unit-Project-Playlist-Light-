63361c22cc427ab6e74315031568543022f455a5
GlastYT <80287109+GlastYT@users.noreply.github.com>
2023-01-31 00:19:57 +0000

    I added a lot of stuff trust

--- script.js
+++ script.js
@@ -12,18 +12,17 @@ let add = document.querySelector(".add");
 
 
 // task 6: declare variables for your display divs: the image url, song name, artist, and song link. Go back to the HTML to check that you are using the correct class names.
-let imageDisplay = document.querySelector(".column display-image");
-let songNameDisplay = document.querySelector("column display-song");
-let artistDisplay = document.querySelector("column display-artist");
-let songLinkDisplay = document.querySelector("column display-link");
+let imageDisplay = document.querySelector(".column.display-image");
+let songNameDisplay = document.querySelector(".column.display-song");
+let artistDisplay = document.querySelector(".column.display-artist");
+let songLinkDisplay = document.querySelector(".column.display-link");
 
 // task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
-/* All Arrays:
-const imageUrls = ["https://i.ytimg.com/vi/goqqohUitmw/maxresdefault.jpg", "https://assets.teenvogue.com/photos/618de539a6fc4c30472f6c10/16:9/w_4095,h_2303,c_limit/Gracie%20(39)%20-%20by%20Vince%20Aung.jpg", "https://i.ytimg.com/vi/eg2KWIpZW-U/maxresdefault.jpg", "https://i.ytimg.com/vi/gWdjDwYuejI/maxresdefault.jpg", "https://images.genius.com/3b2a66301e2d8cc545278f52affe41bd.1000x1000x1.jpg", "https://i.ytimg.com/vi/odWKEfp2QMY/maxresdefault.jpg"]
-const songNames = ["People Watching", "Feels Like", "Only a Matter of Time", "Till Forever Falls Apart", "Traitor", "THE LONELIEST"]
-const artistNames = ["Conan Gray", "Gracie Abrams", "Joshua Bassett", "Ashe & FINNEAS", "Olivia Rodrigo", "Maneskin"]
-const songLinks = ["https://youtu.be/noJ4I3RBoEY", "https://youtu.be/ptznWGs4GKg", "https://youtu.be/eg2KWIpZW-U", "https://youtu.be/gWdjDwYuejI", "https://youtu.be/CRrf3h9vhp8", "https://youtu.be/odWKEfp2QMY"]
-*/
+/* All Arrays: */
+let imageUrls = ["https://i.ytimg.com/vi/goqqohUitmw/maxresdefault.jpg", "https://assets.teenvogue.com/photos/618de539a6fc4c30472f6c10/16:9/w_4095,h_2303,c_limit/Gracie%20(39)%20-%20by%20Vince%20Aung.jpg", "https://i.ytimg.com/vi/eg2KWIpZW-U/maxresdefault.jpg", "https://i.ytimg.com/vi/gWdjDwYuejI/maxresdefault.jpg", "https://images.genius.com/3b2a66301e2d8cc545278f52affe41bd.1000x1000x1.jpg", "https://i.ytimg.com/vi/odWKEfp2QMY/maxresdefault.jpg"]
+let songNames = ["People Watching", "Feels Like", "Only a Matter of Time", "Till Forever Falls Apart", "Traitor", "THE LONELIEST"]
+let artistNames = ["Conan Gray", "Gracie Abrams", "Joshua Bassett", "Ashe & FINNEAS", "Olivia Rodrigo", "Maneskin"]
+let songLinks = ["https://youtu.be/noJ4I3RBoEY", "https://youtu.be/ptznWGs4GKg", "https://youtu.be/eg2KWIpZW-U", "https://youtu.be/gWdjDwYuejI", "https://youtu.be/CRrf3h9vhp8", "https://youtu.be/odWKEfp2QMY"]
 
 //REFACTOR ARRAYS DAY 
 // task 11: comment out the arrays data.
@@ -36,8 +35,7 @@ const Traitor = {songName: "Traitor", imageUrl: "https://images.genius.com/3b2a6
 const TheLoneliest = {songName: "THE LONELIEST", imageUrl: "https://i.ytimg.com/vi/odWKEfp2QMY/maxresdefault.jpg", arist: "Maneskin", songLink: "https://youtu.be/odWKEfp2QMY"}
 // task 13: inside each object, add key/value pairs to store the image url, song name, artist, and song link.
 // task 14: create an array that stores all of the objects.
-
-
+const allSongs = [PeopleWatching, FeelsLike, OAMOT, TFFA, Traitor, TheLoneliest]
 
 //REFACTOR LOOPS DAY 
 // task 15: update your `addSongInfo` function so the input values are saved in as values in a new object.
@@ -51,10 +49,17 @@ const TheLoneliest = {songName: "THE LONELIEST", imageUrl: "https://i.ytimg.com/
 function addSongInfo() {
 
   // task 9: declare a variable to save the user input of the image url. Declare three more variables that save user input: One for the song names, one for the artists, and a last one for the song links.
-
+inputImageUrl = image.value;
+inputSongName = songName.value;
+inputArtistName = artist.value;
+inputSongLink = songLink.value;
+console.log(inputImageUrl, inputSongName, inputArtistName, inputSongLink)
 
   // task 10: use `.push()` to add each input value to the correct array.
-
+imageUrls.push(inputImageUrl)
+songNames.push(inputSongName)
+artistNames.push(inputArtistName)
+songLinks.push(inputSongLink)
 }
 
 
@@ -62,21 +67,32 @@ function addSongInfo() {
 
 /******** this function empties the display divs each time the button is clicked so that your playlist does not repeatedly add the data too many times. Where should this function be placed???********/
 function emptyDisplay() {
-  displayImage.innerHTML = "";
-  displaySong.innerHTML = "";
-  displayArtist.innerHTML = "";
-  displayLink.innerHTML = "";
+  imageDisplay.innerHTML = "";
+  songNameDisplay.innerHTML = "";
+  artistDisplay.innerHTML = "";
+  songLinkDisplay.innerHTML = "";
 }
 
 
 
 
 function displaySongInfo() {
-
   // task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
-
-
-
+for (let i = 0; i < imageUrls.length; i++) {
+  let imageSource = document.createElement("img")
+  imageSource.src = imageUrls[i]
+  imageDisplay.appendChild(imageSource)
+  console.log("This function works!")
+  let songSource = document.createElement("p")
+  songSource.append(songNames[i])
+  songNameDisplay.append(songSource)
+  let artistSource = document.createElement("p")
+  artistSource.append(artistNames[i])
+  artistDisplay.append(artistSource)
+  let songLinkSource = document.createElement("a")
+  songLinkSource.append(songLinks[i])
+  songLinkDisplay.append(songLinkSource)  
+}
 }
 
 
@@ -85,6 +101,7 @@ function displaySongInfo() {
 
 // click event to add and display songs
 add.onclick = function() {
+  emptyDisplay();
   addSongInfo();
   displaySongInfo();
 };
